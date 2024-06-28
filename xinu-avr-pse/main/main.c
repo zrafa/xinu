fd3c554a305e3fc77a42fe3235d96c68e7b862a8
Rafael Ignacio Zurita <rafa@fi.uncoma.edu.ar>
2024-06-28 18:27:36 -0300

    simple

--- xinu-avr-pse/main/main.c
+++ xinu-avr-pse/main/main.c
@@ -1,20 +1,5 @@
-#include <xinu.h>
-#include "serial.h"
 
-
-int main(void)
-{
-	int n = 0;
-
-	serial_init();
-	while(1) {
-		sleepms(1000);
-		n++;
-		serial_put_str("hola \n\r");
-		serial_put_int(n, 4);
-		serial_put_str("\n\r");
-
-	}
+void main(void) {
+	for(;;);
 }
-
 
